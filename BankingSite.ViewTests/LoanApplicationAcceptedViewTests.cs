a01fb6f66e1a47a95685655ec7b2d483f97c601d
Muhammad JAQMAQJI <mohjak@outlook.com>
2016-01-15 09:07:23 +0200

    View Tests for Loan Application Accepted View.

---
+++ BankingSite.ViewTests/LoanApplicationAcceptedViewTests.cs
@@ -0,0 +1,32 @@
+﻿using BankingSite.Models;
+using HtmlAgilityPack;
+using NUnit.Framework;
+using RazorGenerator.Testing;
+
+namespace BankingSite.ViewTests
+{
+	[TestFixture]
+	public class LoanApplicationAcceptedViewTests
+	{
+		[Test]
+		public void ShouldRenderCorrectApplicationDetails()
+		{
+			var sut = new Views.LoanApplication.Accepted();
+
+			var model = new LoanApplication
+			{
+				ID = 99,
+				Age = 33,
+				FirstName = "Gentry",
+				LastName = "Smith",
+				AnnualIncome = 12345.67m
+			};
+
+			HtmlDocument html = sut.RenderAsHtml(model);
+
+			var renderedFirstName = html.GetElementbyId("firstName").InnerText;
+
+			Assert.That(renderedFirstName,Is.EqualTo("Gentry"));
+		}
+	}
+}
